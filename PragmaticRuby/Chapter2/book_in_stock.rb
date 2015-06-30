57a19a42380e4808c9a1e189112df34535a41c66
0oneo <neoman.v@gmail.com>
2015-06-30 22:15:57 +0800

    complete csv demo

---
+++ PragmaticRuby/Chapter2/book_in_stock.rb
@@ -0,0 +1,15 @@
+
+class BookInStock
+
+  attr_reader :isbn
+  attr_accessor :price
+
+  def initialize(isbn, price)
+    @isbn = isbn
+    @price = Float(price)
+  end
+
+  def to_s
+    "ISBN: #{@isbn}, price: #{@price}"
+  end
+end
