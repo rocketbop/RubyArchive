68bb3a6eb2db75e8bc534c6b7fe6f7b33761bca1
0oneo <neoman.v@gmail.com>
2015-07-01 20:34:11 +0800

    add laze enumerator
    lazy enumerator don't consume any data, the data is calculated when you
    request for it

--- PragmaticRuby/Chapter 4/enumerator.rb
+++ PragmaticRuby/Chapter 4/enumerator.rb
@@ -5,6 +5,7 @@ triangular_number = Enumerator.new do |yielder|
   loop do
     number += count
     count += 1
+    puts "The 1 Iterator: #{number}"
     yielder.yield number
   end
 end
@@ -13,13 +14,37 @@ class Enumerator
   def infinite_select(&block)
     Enumerator.new do |yielder|
       self.each do |value|
-        yielder.yield value if block.call value
+        puts "select #{value}"
+        if block.call value
+          yielder.yield value
+        end
       end
     end
   end
 end
 
 p triangular_number
-      .infinite_select { |val| val % 10 == 0 }
-      .infinite_select { |val| val.to_s =~ /3/ }
-      .first(5)
+      .infinite_select { |val| puts "in select 1"; val % 10 == 0 }
+      .infinite_select { |val| puts "in select 2"; val.to_s =~ /3/ }
+      .first(1)
+
+triangular_number.first 10
+
+def palindrome?(n)
+  n = n.to_s
+  n == n.reverse
+end
+
+def Integer.all
+  Enumerator.new do |yielder, n: 0|
+    loop { yielder.yield(n += 1)}
+  end.lazy
+end
+
+p Integer
+        .all
+        .select {|i| (i % 3).zero? }
+        .select {|i| palindrome?(i) }
+        .first(10)
+
+
