b8b320e57e2869eeb782664ba1f773fa09166e8f
0oneo <neoman.v@gmail.com>
2015-07-08 22:36:25 +0800

    use module as mixin

--- PragmaticRuby/Chapter 5/test_mixin.rb
+++ PragmaticRuby/Chapter 5/test_mixin.rb
@@ -31,3 +31,22 @@ et = EightTrack.new("Surrealistic Pillow")
 
 puts ph.who_am_i?
 puts et.who_am_i?
+
+class VowelFinder
+  include Enumerable
+
+  def initialize(string)
+    @string = string
+  end
+
+  def each
+    @string.scan(/[aeiou]/) do |vowel|
+      yield vowel
+    end
+  end
+end
+
+vf = VowelFinder.new("the quick brown fox jumped")
+puts vf.inject(:+)
+
+puts vf.instance_of? Enumerable
